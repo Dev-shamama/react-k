67fb3a2d6fe5f78ed478d024330dc9a3838029bb
shamama <rohishamama@gmail.com>
2024-08-10 15:58:53 +0500

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,63 @@
 import { useEffect, useState } from "react";
-const App = () => {
-  const [count, setCount] = useState([]);
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 
-  const getData = async () => {
-    const res = await fetch("http://api.github.com/users");
-    const result = await res.json();
-    setCount(result);
-  };
+const Home = () => {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/login">Contact</Link>
+    </>
+  );
+};
 
-  useEffect(() => {
-    getData();
-  }, []);
+const Login = () => {
+  // const navigate = useNavigate();
+  // const [auth, setAuth] = useState("admin");
+
+  // const [show, setShow] = useState(false);
+  // useEffect(() => {
+  // if (!auth) {
+  //   navigate("/");
+  // }
+  // auth === false ? navigate("/"): null
+  // }, [auth, navigate]);
 
   return (
     <>
-      <button>Button</button>
-      {count.map((i, index) => {
-        return <li key={index}>{i.login}</li>;
-      })}
+      {/* <Link to="/">Home</Link>
+      {auth !== "user" ? <Link to="/about">About</Link> : null}
+      <Link to="/login">Login</Link> */}
+
+      {/* <button id="btn" onClick={()=> setShow(!show)}>
+        Button
+      </button> */}
+      {/* Normal Syntax For Condition */}
+      {/* {show ? <input type="text" /> : null} */}
+
+      {/* Best Syntax For Condition */}
+      {/* {show && <input type="text" />} */}
     </>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/login">Login</Link>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </>
 );
